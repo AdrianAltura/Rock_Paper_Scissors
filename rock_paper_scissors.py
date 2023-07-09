a91fbeb362c99ce9cd1c1b09c0b9a5bcceb855d2
AdrianAltura <138424171+AdrianAltura@users.noreply.github.com>
2023-07-09 17:18:58 +0800

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,52 @@
+import random
+
+paper = """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+# 0 = rock, 1 = paper, 2 = scissors
+
+choices = [rock,paper,scissors]
+computer = random.randint(0,2)
+user = int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n'))
+
+if (user == 0 and computer == 2) or (user == 1 and computer == 0) or (user == 2 and computer == 1):
+  print(choices[user])
+  print('Computer chose:')
+  print(choices[computer])
+  print('You won')
+
+elif user == computer:
+  print(choices[user])
+  print('Computer chose:')
+  print(choices[computer])
+  print("It's a tie")
+
+else:
+  print(choices[user])
+  print('Computer chose:')
+  print(choices[computer])
+  print("You lose")
